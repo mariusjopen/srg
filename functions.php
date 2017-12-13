9c0cf2084625157cbd6dee2e9fdb7186000f5f8e
mariusjopen <mail@mariusjopen.com>
2017-12-13 10:25:50 +0100

    prepared functions php first element image

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@ $ver = $theme->get('Version');
 
 remove_action( 'wp_head', 'wp_generator' );
 add_filter( 'show_admin_bar', '__return_false' );
+add_action( 'after_setup_theme', 'image_sizes' );
+
 
 function add_theme_scripts(){
 	global $ver;
@@ -13,7 +15,25 @@ function add_theme_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+// RESPONSIVE IMAGES
 
+function image_sizes(){
+	add_image_size( '_3200', 3200, 0, 0 );
+	add_image_size( '_2560', 2560, 0, 0 );
+	add_image_size( '_1920', 1920, 0, 0 );
+	add_image_size( '_1280', 1280, 0, 0 );
+	add_image_size( '_1024', 1024, 0, 0 );
+	add_image_size( '_768', 768, 0, 0 );
+	add_image_size( '_512', 512, 0, 0 );
+	add_image_size( '_256', 256, 0, 0 );
+}
+// ACF CONTROL
+
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page();
+
+}
 
 
 ?>
---
+++ inc/image-big.php
@@ -0,0 +1,14 @@
+<!-- START IMAGE BIG -->
+<?php if( $image_big ): ?>
+
+	<div class="image-full">
+	  <?php
+	  $size = '_2560';
+	  if( $image_big ) {
+	    echo wp_get_attachment_image( $image_big, $size );
+	  }
+	  ?>
+	</div>
+
+<?php endif; ?>
+<!-- END IMAGE BIG -->
