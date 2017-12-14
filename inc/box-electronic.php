3ff20b6ca3f566083079346089d4dd8f962b5966
mariusjopen <mail@mariusjopen.com>
2017-12-14 18:24:21 +0100

    box electronic done

--- inc/box-electronic.php
+++ inc/box-electronic.php
@@ -2,11 +2,28 @@
 <div class="box electronic">
 
 	<div class="left">
-		lalala
+		<div class="headline">
+			<h2><?php echo get_field('uberschrift_electronic'); ?></h2>
+		</div>
+
+		<div class="logo-electronic">
+			<img src="<?php echo get_field('logo_electronic'); ?>">
+		</div>
 	</div>
 
 	<div class="right">
-		lololo
+		<div class="text">
+			<p><?php echo get_field('text_electronic'); ?></p>
+		</div>
+
+		<div class="link">
+			<p-3>
+				<?php
+				$page_link = get_field('link_electronic', false, false);
+				include(locate_template('inc/page-link.php'));
+				?>
+			</p-3>
+		</div>
 	</div>
 
 </div>
