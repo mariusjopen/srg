e1e87d4cc6b9ddaf4e640dc84646901e32954df3
mariusjopen <mail@mariusjopen.com>
2017-12-16 10:28:32 +0100

    created dienstleistungs page

---
+++ inc/box-navigation-blocks.php
@@ -0,0 +1,9 @@
+<!-- START BOX GRAFIK -->
+<div class="box grafik">
+	<div class="center">
+		<div class="top">
+			<h3><?php echo get_field('navigation_text'); ?></h3>
+		</div>
+	</div>
+</div>
+<!-- END BOX GRAFIK -->
---
+++ page-dienstleistungen.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<?php
+include(locate_template('inc/box-navigation-blocks.php'));
+?>
+
+
+<?php get_footer(); ?>
